3b3ce5edffdef8996a053e119688ebac18f7dcab
Alexander Kal <alexjkal@gmail.com>
2025-06-11 23:12:15 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,11 +1,27 @@
 hcl
-#this script configures the Okta provider
+
+#this script configures the Okta provider so that it knows how to interact with Okta
+
+terraform {
+  required_providers {
+    okta = {
+      source = "okta/okta"
+      }
+    }
+  }
+
+#create the custom application in okta. collect clientID and generate new secret.
+#download the .pem version, then convert into an RSA key.
+###openssl rsa -in <path_to_.pem> -out <rsa.pem>
+#
 
 #configure the okta provider
 provider "okta" {
   org_name  = "<your_okta_org_name>"
   base_url  = "okta.com"
   api_token = "<your_okta_api_token>"
+  client_id = "<clientID>"
+  private_key = "${path.module}/rsa.pem"
 }
 
 #data source for the Okta Dashboard app
@@ -13,6 +29,7 @@ data "okta_app" "okta_dashboard" {
   label = "Okta Dashboard"
 }
 
+#use the resource command to change attributes
 resource "okta_user" "alex.kal" {
   first_name = "Alex"
   last_name  = "Kal"
@@ -25,6 +42,18 @@ resource "okta_group_memberships" "admin_membership" {
   users    = [okta_user.alex.kal.id]
 }
 
+resource "okta_group" "friends" {
+  name = "friends"
+  description = "created by Terraform."  
+}
+
+
+#run terraform init to intialize the .tf file and validate the providers
+#if this fails, double check the attributes
 terraform init
+
+#run terraform plan to stage the changes you want to make to the infrastructure
 terraform plan 
+
+#once you review the changes, accept the changes by entering yes
 terraform apply
